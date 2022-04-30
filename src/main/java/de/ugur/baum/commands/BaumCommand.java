4fefa3f7105a413375aa628421d31b21c9e68e21
Uguralg3a <58608648+Uguralg3a@users.noreply.github.com>
2022-04-30 15:12:02 +0200

    Update BaumCommand.java

--- src/main/java/de/ugur/baum/commands/BaumCommand.java
+++ src/main/java/de/ugur/baum/commands/BaumCommand.java
@@ -1,2 +1,35 @@
-package de.ugur.baum.commands;public class BaumCommand {
+package de.ugur.baum.commands;
+
+import de.ugur.baum.Baum;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class BaumCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (sender instanceof Player) {
+
+            Player p = (Player) sender;
+
+            if (p.hasPermission("baum.command.baumcommand")) {
+
+                p.sendMessage(Baum.prefix + "§9Baum Plugin");
+                p.sendMessage(Baum.prefix + "§9Version 1.0.0");
+                p.sendMessage(Baum.prefix + "§9Developed by Ugur");
+
+            } else {
+                p.sendMessage(Baum.NoPerm);
+            }
+
+        } else {
+            sender.sendMessage(Baum.NoPlayer);
+        }
+
+        return false;
+    }
 }
